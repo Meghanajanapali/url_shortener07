2250014f1cbbba9dbac9e1b730db5b530d563363
MeghanaJanapali <meghanayadavjanapali@gmail.com>
2025-07-06 13:54:29 +0530

    backend changes

---
+++ backend/src/controllers/shortUrlController.js
@@ -0,0 +1,57 @@
+import {ShortURL} from "../models/shorturl.model.js" 
+import { nanoid } from 'nanoid'
+export const shortUrl= async (req,res)=>{
+    try{
+        const userId=req.user.id;
+
+        const {originalUrl, expiresAt, title, customUrl} =req.body
+
+        if(!originalUrl){
+            return res.status(400).send({status: "missing originalUrl in payload"})
+        }
+
+        let shortCode=""
+        if(customUrl){
+            shortCode=customUrl;
+            let existData=await ShortURL.findOne({shortCode});
+            if(existData) return res.status(400).send({status: "Bad request"})
+        }
+        else{
+            shortCode=nanoid(7);
+            let isUnique=false;
+            while(!isUnique){
+                const existData=await ShortURL.findOne({shortCode});
+
+                if(!existData) isUnique=true;
+                else shortCode=nanoid(7);
+            }
+
+        }
+        const  newURL=new ShortURL({
+            originalUrl,
+            shortCode,
+            userId,
+
+        })
+        await newURL.save();
+        return res.status(200).send(newURL);
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({status:"Internal server Error"});
+    }
+}
+
+export const redirectFunction= async(req, res)=>{
+    try{
+        const shortCode= req.params.shortcode;
+        const data=await ShortURL.findOne({shortCode});
+
+        if(!data){
+            return res.status(404).send({status:"Not Found"});
+        }
+        return res.redirect(data.originalUrl);
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({status:"Internal Server Error"});
+    }
+}
--- backend/src/routes/shortURLRouter.js
+++ backend/src/routes/shortURLRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { redirectFunction, shortUrl } from "../controllers/shortUrlController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const shortURLRouter = Router();
 
-
-
+shortURLRouter.post("", protect, shortUrl)
+shortURLRouter.get("/:shortcode", redirectFunction)
 export default shortURLRouter;
